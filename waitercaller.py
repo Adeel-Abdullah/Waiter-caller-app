f67da038c4e99393736ac3acc66078feed078dca
IC-HO-2032 <syedabdullah94@hotmail.com>
2017-10-07 23:13:39 +0500

    added user registration

--- waitercaller.py
+++ waitercaller.py
@@ -1,13 +1,14 @@
 from __future__ import print_function
-import sys
 from flask import Flask
 from flask.ext.login import LoginManager, login_required, login_user, logout_user
 from flask import render_template, redirect, url_for, request
 
 from mockdbhelper import MockDBHelper as DBHelper
 from user import User
+from passwordhelper import PasswordHelper
 
 DB = DBHelper()
+PH = PasswordHelper()
 
 app = Flask(__name__)
 app.secret_key = 'eQjBXmwhGYsw2pNshN938WPPWA+JxV+95GIvNGEEVNSLRBjeROx+6Jkne4yFrhdEspat7TK8nMVz\
@@ -25,12 +26,11 @@ def account():
 
 @app.route("/login", methods =["POST"])
 def login():
-    email=request.form.get("email")
-    password=request.form.get("password")
-    user_password=DB.get_user(email)
-    
-    if user_password and user_password == password:
-        user=User(email)
+    email = request.form.get("email")
+    password = request.form.get("password")
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
+        user = User(email)
         login_user(user, remember=True)
         return redirect(url_for('account'))
     return home()
@@ -40,11 +40,25 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed= PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
+
+
 @login_manager.user_loader
 def load_user(user_id):
-    user_password=DB.get_user(user_id)
+    user_password = DB.get_user(user_id)
     if user_password:
         return User(user_id)
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=False)
+    app.run(port=5000, debug=False)
